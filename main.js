423a628a3a32fd3b4d45e1d17b7dfcea9ab60f04
raghdarizq <raghdarizq2@gmail.com>
2023-03-02 13:59:10 +0200

    add edit func > Relates  #7

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const desc = document.getElementById("Desc");
 const date = document.getElementById("date");
 const submit = document.getElementById("submit");
 const todo = [];
+let mediator;
+let Mood;
 
 // sumbit data
 submit.onclick = function (eo) {
@@ -12,21 +14,39 @@ submit.onclick = function (eo) {
     desc: desc.value,
     date: date.value,
   };
-  todo.push(newTOdo);
+  if (Mood == "Edit") {
+    console.log("edit");
+    todo[mediator] = newTOdo;
+    submit.value = "submit";
+    Mood = "submit";
+  } else {
+    todo.push(newTOdo);
+  }
+
+  console.log(Mood);
   Read();
 };
-function Read (){
-  let card ="";
-  todo.forEach((element,index)=> {
-    card+=`
+function Read() {
+  let card = "";
+  todo.forEach((element, index) => {
+    card += `
     <li id="list">${element.task}
     <span>${element.desc}</span>
     <span>${element.date}</span>
-    <button id="edit">edit</button>
+    <button id="edit" onclick="edit(${index})">edit</button>
     <button id="delete">Delete</button>
 </li>
-    `
+    `;
   });
-  document.getElementById("sec").innerHTML=card;
+  document.getElementById("sec").innerHTML = card;
 }
- Read();
+Read();
+const edit = (i) => {
+  Mood = "Edit";
+  console.log(mediator);
+  task.value = todo[i].task;
+  desc.value = todo[i].desc;
+  date.value = todo[i].date;
+  submit.value = "Edit";
+  mediator = i;
+};
